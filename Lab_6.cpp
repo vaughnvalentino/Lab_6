6c5aa7d0a0419b93ca6680ac79b0dba37d976e71
Vaughn <vaugh@LAPTOP-FEEBKMTP>
2024-02-27 21:33:07 -0500

    Add project files.

---
+++ Lab_6.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+using namespace std;
+
+int main() {
+	string background = "images1/backgrounds/winter.png";
+	string foreground = "images1/characters/yoda.png";
+
+	Texture backgroundTex;
+	if (!backgroundTex.loadFromFile(background)) {
+		cout << "Couldn't Load Image" << endl;
+		exit(1);
+	}
+	Texture foregroundTex;
+	if (!foregroundTex.loadFromFile(foreground)) {
+		cout << "Couldn't Load Image" << endl;
+		exit(1);
+	}
+
+	Image backgroundImage;
+	backgroundImage = backgroundTex.copyToImage();
+	Image foregroundImage;
+	foregroundImage = foregroundTex.copyToImage();
+	Vector2u sz = backgroundImage.getSize();
+
+	Color gscreen; 
+	for (int y = 0; y < sz.y; y++) {
+		for (int x = 0; x < sz.x; x++) {
+			if (y == 0 && x == 0) {
+				gscreen = foregroundImage.getPixel(x, y);
+			}
+				Color fore = foregroundImage.getPixel(x, y);
+				Color back = backgroundImage.getPixel(x, y);
+				if (fore == gscreen) {
+					foregroundImage.setPixel(x, y, back);
+				}
+		}
+	}
+
+	RenderWindow window(VideoMode(1024, 768), "Here's the output");
+	Sprite sprite1;
+	Texture tex1;
+
+	tex1.loadFromImage(foregroundImage);
+	sprite1.setTexture(tex1);
+	window.clear();
+	window.draw(sprite1);
+	window.display();
+	while (true);
+}
